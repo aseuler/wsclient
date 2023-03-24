5f6bccac809e52551ad1f19c47e69093c6c997df
aseuler <aseuler@outlook.com>
2023-03-24 09:39:13 +0800

    remove future dependency

--- src/bin/wsclient.rs
+++ src/bin/wsclient.rs
@@ -1,15 +1,10 @@
-use futures::executor::block_on;
 use std::env;
 use wsclient;
 
 #[tokio::main]
-pub async fn run(url: String) {
-    wsclient::WsClient::new(url).chat().await;
-}
-
-pub fn main() {
+pub async fn main() {
     let url = env::args()
         .nth(1)
         .unwrap_or_else(|| panic!("this program requires at least one argument"));
-    block_on(async { run(url) });
+    wsclient::WsClient::new(url).chat().await;
 }
